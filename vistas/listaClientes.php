d16d8db0d60b596d2b242257d66faf148fc5ac6c
Marcos Ginel <marcos.ginel@gmail.com>
2016-01-18 00:43:48 +0100

    Añadida tabla con jquery, buscador, orden y paginado.

--- vistas/listaClientes.php
+++ vistas/listaClientes.php
@@ -5,7 +5,10 @@ session_start(); // Iniciar la sesion
 <html>
 <head>
 <title>Telefoneitor</title>
-<link href="style.css" rel="stylesheet" type="text/css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="../css/jquery.dataTables.css" rel="stylesheet" type="text/css">
+    <script src="../js/jquery.js"></script>
+    <script src="../js/jquery.dataTables.js"></script>
 </head>
 <body>
 <header>
@@ -21,10 +24,40 @@ session_start(); // Iniciar la sesion
     include_once($_SERVER['DOCUMENT_ROOT'] . "/repositorio/repositorioClientes.php");
     $username = $_SESSION['username'];
     $password = $_SESSION['password'];
+    echo "<table id=\"example\" class=\"display\" cellspacing=\"0\" width=\"100%\">";
+    echo "<thead>
+            <tr>
+                <th>Nombre</th>
+                <th>DNI</th>
+                <th>Telefono</th>
+                <th>Email</th>
+            </tr>
+        </thead>";
+    echo "<tfoot>
+            <tr>
+                <th>Nombre</th>
+                <th>DNI</th>
+                <th>Telefono</th>
+                <th>Email</th>
+            </tr>
+        </tfoot>";
     $array = getClientes($username, $password);
-    foreach ($array as $clave => $valor)
-       $valor->imprimeCliente();
+    echo "<tbody>";
+    foreach ($array as $clave => $valor) {
+        echo "<tr>";
+        echo "<td>".$valor->getNombre()."</td>";
+        echo "<td>".$valor->getDNI()."</td>";
+        echo "<td>".$valor->getTelefono()."</td>";
+        echo "<td>".$valor->getEmail()."</td>";
+        echo "</tr>";
+    }
+    echo "</tbody></table>";
 ?>
 </div>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        } );
+    </script>
 </body>
 </html>
