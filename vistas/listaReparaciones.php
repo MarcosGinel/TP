2fba0fa9cf06f81fbcd36d8999491afd97944ece
omakrivic <omimar91@hotmail.com>
2016-01-17 20:34:24 +0100

    Create listaReparaciones.php

---
+++ vistas/listaReparaciones.php
@@ -0,0 +1,18 @@
+<?php
+session_start(); // Iniciar la sesion
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Telefoneitor</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="main">
+<h1>Telefoneitor</h1>
+<h2>Lista de Reparaciones:</h2>
+<?php
+?>
+</div>
+</body>
+</html>
